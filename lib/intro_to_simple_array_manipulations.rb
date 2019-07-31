4f98a3edbcc7fb276f2b8d86f24de53a261dc040
Tate Shepard <tateshepard@yahoo.com>
2019-07-31 02:37:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -46,6 +46,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array, 4)
-  array.delete(4)
+def using_delete_at(array, integer)
+  array.delete(integer, integer)
 end
